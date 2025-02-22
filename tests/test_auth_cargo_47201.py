dd871b4790ad5a8c164cdd781128bfb8fccb807b
Anton Sobjanin <sobjaninav@gmail.com>
2025-02-22 14:38:17 +0500

    settings added

--- tests/test_auth_cargo_47201.py
+++ tests/test_auth_cargo_47201.py
@@ -2,17 +2,12 @@ import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def test_auth_caro_47201(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+def test_auth_caro_47201(page):
     page.goto("https://servicetest.b2b-logist.com/cargo/47201/ru_RU/")
     page.locator("#userName").click()
     page.locator("#userName").fill("472ulsu@gmail.com")
     page.locator("#userPassword").click()
     page.locator("#userPassword").fill("123")
     page.get_by_role("button", name="Войти").click()
+    page.wait_for_timeout(30000)
 
-    # ---------------------
-    #context.close()
-    #browser.close()
